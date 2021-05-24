ccb4d1dfb0cb1968fea3695d4f377a6a2798a26b
karolina <nogacka.karolina@gmail.com>
2021-05-24 20:08:06 +0200

    make some corrects

--- bitStream/FileBinaryIO.py
+++ bitStream/FileBinaryIO.py
@@ -1,4 +1,5 @@
 import bitarray
+import png
 
 
 class FileIO:
--- main.py
+++ main.py
@@ -25,67 +25,6 @@ def test_bpsk(noise_strength):
     print(out_bits)
 
 
-def test_bpsk_picture(noise_strength):
-    # tests sending picture from computerA to computerB with bpsk
-    modulator = Modulator()
-    demodulator = Demodulator()
-    channel = Channel()
-
-    # send signal to modulator
-    bits = FileIO("computerA\\cloud.png").read_from_file()
-    print(bits)
-    signal = modulator.make_bpsk_mod(bits)
-    signal.show_signal()
-
-    # send signal to channel
-    signal = channel.send_signal(signal, noise_strength)
-
-    # demodulator receives signal
-    result_bits = demodulator.make_bpsk_demod(signal, channel)
-
-    # picture_pbm_result = PbmClass()
-    # picture_pbm_result.read_wireless_signal_from_bits(result_bits)
-    # picture_pbm_result.rows = picture_pbm.rows
-    # picture_pbm_result.columns = picture_pbm.columns
-    # picture_pbm_result.bits = result_bits
-    #picture_pbm_result.write_pbm("computerB\\recieved_bpsk.pbm")
-    FileIO("computerB\\cloud_bpsk.png").write_to_file(result_bits)
-
-
-def test_pbm_mul():
-    # tests magnifying pbm
-    picture_pbm = PbmClass()
-    picture_pbm.read_pbm("computerA\\small.pbm")
-    picture_pbm.multiply_pbm(10, "computerA\\10_times_small.pbm")
-
-
-def test_qpsk_picture(noise_strength):
-    # tests sending pbm from computerA to computerB with bpsk
-    picture_pbm = PbmClass()
-    picture_pbm.read_pbm(file_name="computerA\\10_times_small.pbm")
-    modulator = Modulator()
-    demodulator = Demodulator()
-    channel = Channel()
-
-    # send signal to modulator
-    bits = picture_pbm.write_wireless_signal_to_bits()
-    signal = modulator.make_qpsk_mod(bits)
-    signal.show_signal()
-
-    # send signal to channel
-    signal = channel.send_signal(signal, noise_strength)
-
-    # demodulator receives signal
-    result_bits = demodulator.make_qpsk_demod(signal, channel)
-
-    picture_pbm_result = PbmClass()
-    picture_pbm_result.read_wireless_signal_from_bits(result_bits)
-    # picture_pbm_result.rows = picture_pbm.rows
-    # picture_pbm_result.columns = picture_pbm.columns
-    # picture_pbm_result.bits = result_bits
-    picture_pbm_result.write_pbm("computerB\\recieved_qpsk.pbm")
-
-
 def test_qpsk(noise_strength):
     lista = [1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1]
     modulator = Modulator()
@@ -131,43 +70,60 @@ def test_16psk(noise_strength):
     print("Komputer B: ", bits)
 
 
-def test_16psk_picture(noise_strength):
-    # tests sending pbm from computerA to computerB with bpsk
-    picture_pbm = PbmClass()
-    picture_pbm.read_pbm(file_name="computerA\\10_times_small.pbm")
+def test_bpsk_picture(noise_strength):
+    # tests sending picture from computerA to computerB with bpsk
     modulator = Modulator()
     demodulator = Demodulator()
     channel = Channel()
 
     # send signal to modulator
-    bits = picture_pbm.write_wireless_signal_to_bits()
-    signal = modulator.make_16psk_mod(bits)
+    bits = FileIO("computerA\\cloud.png").read_from_file()
+    print(bits)
+    signal = modulator.make_bpsk_mod(bits)
     signal.show_signal()
 
     # send signal to channel
     signal = channel.send_signal(signal, noise_strength)
 
     # demodulator receives signal
-    result_bits = demodulator.make_16psk_demod(signal, channel)
+    result_bits = demodulator.make_bpsk_demod(signal, channel)
+
+    FileIO("computerB\\cloud_bpsk.png").write_to_file(result_bits)
+
+
+def test_qpsk_picture(noise_strength):
+    # tests sending png from computerA to computerB with qpsk
+    modulator = Modulator()
+    demodulator = Demodulator()
+    channel = Channel()
+
+    # send signal to modulator
+    bits = FileIO("computerA\\img.png").read_from_file()
+    signal = modulator.make_qpsk_mod(bits)
+    signal.show_signal()
+
+    # send signal to channel
+    signal = channel.send_signal(signal, noise_strength)
+
+    # demodulator receives signal
+    result_bits = demodulator.make_qpsk_demod(signal, channel)
 
     picture_pbm_result = PbmClass()
     picture_pbm_result.read_wireless_signal_from_bits(result_bits)
     # picture_pbm_result.rows = picture_pbm.rows
     # picture_pbm_result.columns = picture_pbm.columns
     # picture_pbm_result.bits = result_bits
-    picture_pbm_result.write_pbm("computerB\\recieved_16psk.pbm")
+    FileIO("computerB\\recieved_qpsk_img.png").write_to_file(result_bits)
 
 
 def test_8psk_picture(noise_strength):
-    # tests sending pbm from computerA to computerB with bpsk
-    picture_pbm = PbmClass()
-    picture_pbm.read_pbm(file_name="computerA\\10_times_small.pbm")
+    # tests sending png from computerA to computerB with 8psk
     modulator = Modulator()
     demodulator = Demodulator()
     channel = Channel()
 
     # send signal to modulator
-    bits = picture_pbm.write_wireless_signal_to_bits()
+    bits = FileIO("computerA\\img.png").read_from_file()
     signal = modulator.make_8psk_mod(bits)
     signal.show_signal()
 
@@ -179,15 +135,33 @@ def test_8psk_picture(noise_strength):
 
     picture_pbm_result = PbmClass()
     picture_pbm_result.read_wireless_signal_from_bits(result_bits)
-    # picture_pbm_result.rows = picture_pbm.rows
-    # picture_pbm_result.columns = picture_pbm.columns
-    # picture_pbm_result.bits = result_bits
-    picture_pbm_result.write_pbm("computerB\\recieved_8psk.pbm")
+    FileIO("computerB\\recieved_8psk_img.png").write_to_file(result_bits)
+
+def test_16psk_picture(noise_strength):
+    # tests sending png from computerA to computerB with 16psk
+    modulator = Modulator()
+    demodulator = Demodulator()
+    channel = Channel()
+
+    # send signal to modulator
+    bits = FileIO("computerA\\img.png").read_from_file()
+    signal = modulator.make_16psk_mod(bits)
+    signal.show_signal()
+
+    # send signal to channel
+    signal = channel.send_signal(signal, noise_strength)
+
+    # demodulator receives signal
+    result_bits = demodulator.make_16psk_demod(signal, channel)
+
+    picture_pbm_result = PbmClass()
+    picture_pbm_result.read_wireless_signal_from_bits(result_bits)
+    FileIO("computerB\\recieved_16psk_img.png").write_to_file(result_bits)
 
 
 # Try sending PNG image through channel using qpsk
 def qpsk_send_png(noise_strength):
-    bit_list = FileIO('./computerA/cloud.png').read_from_file()
+    bit_list = FileIO("computerA\\cloud.png").read_from_file()
     modulator = Modulator()
     demodulator = Demodulator()
     channel = Channel()
@@ -199,11 +173,10 @@ def qpsk_send_png(noise_strength):
 
     result_bits = demodulator.make_qpsk_demod(signal, channel)
 
-    FileIO('./computerB/cloud.png').write_to_file(result_bits)
+    FileIO("computerB\\cloud.png").write_to_file(result_bits)
 
 
-# qpsk_send_png(0.06)  # Try sending png file through the channel
-noise_strength = 0.13
+noise_strength = 0.03
 # test_bpsk(noise_strength)
 # test_bpsk_picture(noise_strength)
 # test_qpsk(noise_strength)
